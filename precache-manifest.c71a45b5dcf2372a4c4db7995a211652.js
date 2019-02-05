935a70e728259f97fa98610ca6e37872a4192785
Jonatan Barr <jonybh99@gmail.com>
2019-02-05 12:36:53 +0200

    Updates

---
+++ precache-manifest.c71a45b5dcf2372a4c4db7995a211652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bc0ac809c6b092990bd",
+    "url": "/memo-students/static/css/main.c3be05f1.chunk.css"
+  },
+  {
+    "revision": "0bc0ac809c6b092990bd",
+    "url": "/memo-students/static/js/main.0bc0ac80.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/memo-students/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "1cb7295a49c3f50df4b5",
+    "url": "/memo-students/static/js/runtime~main.1cb7295a.js"
+  },
+  {
+    "revision": "169dec8eb13b2228a5a92a1887b5a727",
+    "url": "/memo-students/index.html"
+  }
+];
